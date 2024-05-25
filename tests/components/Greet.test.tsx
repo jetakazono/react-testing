bc321c34c6bdc7a6db65631f2325cdaf0bcdee4a
jetakazono <jessica.takazono@gmail.com>
2024-05-25 16:14:15 +0100

    add greet test

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react"
+import Greet from "../../src/components/Greet"
+import "@testing-library/jest-dom/vitest"
+
+describe("Greet", () => {
+    it("should render Hello with the name when name is provided", () => {
+        render(<Greet name="Jessica" />)
+
+        const heading = screen.getByRole("heading")
+
+        expect(heading).toBeInTheDocument()
+        expect(heading).toHaveTextContent(/jessica/i)
+    })
+    it("should render login button when name is not provided", () => {
+        render(<Greet />)
+
+        const button = screen.getByRole("button")
+
+        expect(button).toBeInTheDocument()
+        expect(button).toHaveTextContent(/login/i)
+    })
+})
